897951a9db2210d7039a841a4c788085727acb45
jpbranson <jpbranson@jpbransons-iMac.local>
2022-03-10 15:46:51 -0600

    fix function name

--- R/uncletopia.R
+++ R/uncletopia.R
@@ -1,2 +1,2 @@
-download.file("https://uncletopia.com/api/servers", destfile = paste0("data/uncletopia/", dmake.names(Sys.time()), ".json"))
+download.file("https://uncletopia.com/api/servers", destfile = paste0("data/uncletopia/", make.names(Sys.time()), ".json"))
 
