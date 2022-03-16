b7df0f9a206c9ffc953e4517f8159fcf79ee69c0
jpbranson <jpbranson@jpbransons-iMac.local>
2022-03-16 17:17:49 -0500

    url for mata

---
+++ R/mata.R
@@ -0,0 +1,6 @@
+#https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/topo/vehicules?_tmp=1647467979421
+#https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/horaires/pta/1427098 particular station message or schedule?
+#https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/topo/refresh?_tmp=1647400000 _tmp is unix epoch
+#https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/topo
+#https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/iv/message
+#https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/config/version?_tmp=1647465918008
