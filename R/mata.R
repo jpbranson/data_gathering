6ec5cdd7eafcd8930bec23c773bd59846161c182
jpbranson <jpbranson@jpbransons-iMac.local>
2022-03-17 14:41:07 -0500

    begin mata

--- R/mata.R
+++ R/mata.R
@@ -4,3 +4,15 @@
 #https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/topo
 #https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/iv/message
 #https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/config/version?_tmp=1647465918008
+
+tmp <- paste0(as.character(round(as.numeric(Sys.time()))), "000")
+load("data/stop.id.RData")
+
+
+download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/topo/vehicules?_tmp=", tmp), destfile = paste0("data/mata/", "vehicules-", make.names(Sys.time()), "-", tmp, ".json"))
+download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/topo/refresh?_tmp=", tmp), destfile = paste0("data/mata/", "refresh-", make.names(Sys.time()), "-", tmp, ".json"))
+download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/iv/message?_tmp=", tmp), destfile = paste0("data/mata/", "message-", make.names(Sys.time()), "-", tmp, ".json"))
+
+for(i in seq_along(stop.id)) {
+  download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/horaires/pta/", stop.id[i], "?_tmp="), destfile = paste0("data/mata/", "pta-horaire-", stop.id[i], "-", make.names(Sys.time()), "-", tmp, ".json"))
+}
