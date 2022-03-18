18ca05c78f39df7e3d53dd68ef61471a3dfc32ff
jpbranson <jpbranson@jpbransons-iMac.local>
2022-03-18 10:59:01 -0500

    download brackets

--- R/madness.R
+++ R/madness.R
@@ -1,5 +1,6 @@
 
 if(as.Date(Sys.time()) <= as.Date("2022-04-04")) {
   download.file("https://dl.dropboxusercontent.com/s/j12vjbcpay9haeh/madness.html?dl=1", destfile = paste0("data/madness/", make.names(Sys.time()), ".html"))
+  download.file("https://dl.dropboxusercontent.com/s/z3muhuxx6wybbo0/tenthousand.csv?dl=0", destfile = paste0("data/madness/", make.names(Sys.time()), ".csv"))
 }
 
