669856f26f88fa19ab84e4a953c0712f6e234d56
jpbranson <jpbranson@gmail.com>
2022-04-13 18:49:45 -0500

    stop downloading pta to github

--- R/mata.R
+++ R/mata.R
@@ -7,7 +7,7 @@
 
 tmp <- paste0(as.character(round(as.numeric(Sys.time()))), "000")
 load("data/stop.id.RData")
-
+library(lubridate)
 
 tryCatch(download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/topo/vehicules?_tmp=", tmp), destfile = paste0("data/mata/", "vehicules-", make.names(Sys.time()), "-", tmp, ".json")),
          error = function(e) print(paste("vehicules", 'did not work out')))
@@ -17,10 +17,14 @@ tryCatch(download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/rest
 tryCatch(download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/iv/message?_tmp=", tmp), destfile = paste0("data/mata/", "message-", make.names(Sys.time()), "-", tmp, ".json")),
          error = function(e) print(paste("messages", 'did not work out')))
 
-for(i in seq_along(stop.id)) {
-  tryCatch(
-    download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/horaires/pta/", stop.id[i], "?_tmp=", tmp), destfile = paste0("data/mata/", "pta-horaire-", stop.id[i], "-", make.names(Sys.time()), "-", tmp, ".json")),
-    error = function(e) print(paste("stop.id", stop.id[i], "pta", 'did not work out'))
-  )
+#i need to do something different with this, it'll bog everything down
+# if(minute(Sys.time()) > 30 & minute(Sys.time()) < 45 & hour(Sys.time())) {
+#   for(i in seq_along(stop.id)) {
+#     tryCatch(
+#       download.file(paste0("https://mata.cadavl.com:4437/SWIV/MATA/proxy/restWS/horaires/pta/", stop.id[i], "?_tmp=", tmp), destfile = paste0("data/mata/", "pta-horaire-", stop.id[i], "-", make.names(Sys.time()), "-", tmp, ".json")),
+#       error = function(e) print(paste("stop.id", stop.id[i], "pta", 'did not work out'))
+#     )
+#
+#   }
+# }
 
-}
