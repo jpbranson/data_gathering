1810e613c9cd28a982c40ca4b4083181cf5fff93
jpbranson <jpbranson@jpbransons-iMac.local>
2022-12-02 09:43:53 -0600

    update uncletopia to new endpoint

--- R/uncletopia.R
+++ R/uncletopia.R
@@ -1,6 +1,9 @@
+# tryCatch(
+#   download.file("https://uncletopia.com/api/servers", destfile = paste0("data/uncletopia/", make.names(Sys.time()), ".json")),
+#   error = function(e) print(paste("servers", 'did not work out'))
+#   )
+
 tryCatch(
-  download.file("https://uncletopia.com/api/servers", destfile = paste0("data/uncletopia/", make.names(Sys.time()), ".json")),
+  download.file("https://uncletopia.com/api/servers/state", destfile = paste0("data/uncletopia/", make.names(Sys.time()), "_state.json")),
   error = function(e) print(paste("servers", 'did not work out'))
-  )
-
-
+)
